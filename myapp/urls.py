88558262fef549028fa587158fe83e16e091f558
Yuliia Chebarova-Bobyrcd <ychebarova@gmail.com>
2025-01-29 15:49:15 -0800

    practice creating forms connected to the database

--- myapp/urls.py
+++ myapp/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('ratings', views.ratings),
+    path('', views.form_view, name="home"),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from myapp.forms import MenuForm
+from .models import Menu
+from django.http import JsonResponse
 
-# Create your views here.
+# Add code for form_view() function below
+
+def form_view(request):
+    form = MenuForm()
+    if request.method == 'POST':
+        form = MenuForm(request.POST)
+        print(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            mf = Menu(item_name = cd['item_name'], category = cd['category'], description = cd['description'])
+            mf.save()
+            return JsonResponse({'message': 'success'})
+    return render(request, 'menu_items.html', {'form': form})
+    
